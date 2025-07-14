20f15e5073f354a7fc4c96ba309fbfb57404473b
Javipopotter <153302487+JavipopotterUniversityVersion@users.noreply.github.com>
2025-07-14 08:18:48 +0200

    Casi

--- Scripts/Background_Transitioner.gd
+++ Scripts/Background_Transitioner.gd
@@ -2,9 +2,10 @@ extends TextureRect
 class_name Background_Transitioner
 
 func set_background(new_background:Texture2D):
-	material.set("shader_parameter/texture_1", material.get("shader_parameter/texture_2"))
-	material.set("shader_parameter/texture_2", new_background)
-	animate_shader_param("transform_ratio", 1)
+	material.set("shader_parameter/texture_2", material.get("shader_parameter/texture_1"))
+	material.set("shader_parameter/texture_1", new_background)
+	await animate_shader_param("transform_ratio", 1)
+	#material.set("shader_parameter/texture_1", new_background)
 
 func animate_shader_param(param_name: String, duration: float) -> void:
 	var time := 0.0
@@ -14,4 +15,4 @@ func animate_shader_param(param_name: String, duration: float) -> void:
 		await get_tree().process_frame
 		time += get_process_delta_time()
 	
-	material.set("shader_parameter/" +param_name, 1.0)
+	material.set("shader_parameter/" + param_name, 1.0)
